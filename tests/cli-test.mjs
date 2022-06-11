4633e2e298c26991b3f8e6d6ee18eb66911e14f5
Markus Felten <Markus.Felten@gmx.de>
2022-06-11 11:53:49 +0200

    test: parse properties

--- tests/cli-test.mjs
+++ tests/cli-test.mjs
@@ -6,13 +6,19 @@ const nts = new URL("../src/template-sync-cli.mjs", import.meta.url).pathname;
 test("cli dryrun", async t => {
   const c = await execa(nts, ["--dry", "arlac77/config-expander"]);
 
-  t.truthy(c.stdout.match(/\-|.+:.+/));
+  //t.truthy(c.stdout.match(/\-|.+:.+/));
+  t.truthy(c.stdout.match(/github:arlac77\/config-expander/));
   t.is(c.exitCode, 0);
 });
 
-test("cli list properties", async t => {
+test.only("cli list properties", async t => {
   const c = await execa(nts, ["--list-properties", "arlac77/config-expander"]);
-  t.truthy(c.stdout.match(/Expands expressions in config files/));
-
   t.is(c.exitCode, 0);
+
+  const out = JSON.parse(c.stdout);
+  t.is(out.description, "Expands expressions in config files");
+
+  //t.truthy(c.stdout.match(/Expands expressions in config files/));
+
+  t.log(out);
 });
